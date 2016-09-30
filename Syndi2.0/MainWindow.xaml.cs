2ede3892cf170e3cb23629915fa26f8969b2c7fa
unknown <kausyap10@gmail.com>
2016-09-30 22:32:51 +0530

    Win store to WPF

---
+++ Syndi2.0/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Syndi2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HomePage());
+        }
+
+        public void BackgroundLoaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri("Assets/Icons/SpaceBg.png", UriKind.Relative);
+            b.EndInit();
+            // ... Get Image reference from sender.
+            var image = sender as Image;
+            // ... Assign Source.
+            image.Source = b;
+        }
+
+        private void homeButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new HomePage());
+        }
+
+        private void shareButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new SharePage());
+        }
+        private List<string> exec_cmd(string arguments)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = arguments;
+            process.StartInfo = startInfo;
+            process.Start();
+            List<string> output = new List<string>();
+            output.Add(process.StandardOutput.ReadLine());
+            process.Close();
+            return output;
+        }
+    }
+}
