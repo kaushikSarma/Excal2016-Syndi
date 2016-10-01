77553a66e4a1afda1204c47a91c10149aeda6800
MacBox7 <ajankit2304@gmail.com>
2016-10-02 01:39:23 +0530

    Added share button

--- Syndi2.0/SharePage.xaml.cs
+++ Syndi2.0/SharePage.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
+using System.Windows.Forms;
+using ShareLibrary;
+using System.Text.RegularExpressions;
 namespace Syndi2._0
 {
     /// <summary>
@@ -23,6 +26,50 @@ namespace Syndi2._0
         public SharePage()
         {
             InitializeComponent();
+        }
+
+        private void openBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg1 = new Ionic.Utils.FolderBrowserDialogEx();
+            dlg1.Description = "Select a file or folder";
+            dlg1.ShowNewFolderButton = true;
+            dlg1.ShowEditBox = true;
+            dlg1.ShowBothFilesAndFolders = true;
+            dlg1.ShowFullPathInEditBox = true;
+            dlg1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            // Show the FolderBrowserDialog.
+            DialogResult result = dlg1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var path = dlg1.SelectedPath;
+                var match = Regex.Match(path, @".*\\(.*)$");
+                var name = match.Groups[1].Value;
+                var myPath = "\\\\" + Environment.MachineName + "\\" + name;
+                if (Directory.Exists(dlg1.SelectedPath))
+                {
+                    System.Windows.Forms.MessageBox.Show(myPath);
+                    if(Directory.Exists(@myPath))
+                    {
+                        System.Windows.Forms.MessageBox.Show("File already shared");
+                    }
+                    else
+                    {
+                        if (ShareWin.ShareWithEveryone(dlg1.SelectedPath, name, name))
+                        {
+                            System.Windows.Forms.MessageBox.Show("Success");
+                        }
+                        else
+                        {
+                            System.Windows.Forms.MessageBox.Show("Failure");
+                        }
+                    }
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Not allowed in Windows Environment");
+                }
+            }
         }
     }
 }
