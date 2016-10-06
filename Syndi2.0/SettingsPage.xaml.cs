17a8c21ea31980bf248f3b7a6e9e309fd2c1819d
MacBox7 <ajankit2304@gmail.com>
2016-10-06 23:28:20 +0530

    DefaultPathOption added

--- Syndi2.0/SettingsPage.xaml.cs
+++ Syndi2.0/SettingsPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Syndi2._0
 {
@@ -24,5 +25,30 @@ namespace Syndi2._0
         {
             InitializeComponent();
         }
+
+        private void SetDefaultPath(object sender, RoutedEventArgs e)
+        {
+            var dlg1 = new Ionic.Utils.FolderBrowserDialogEx();
+            TextBlock DefaultPath = new TextBlock();
+            dlg1.Description = "Select a folder";
+            dlg1.ShowNewFolderButton = true;
+            dlg1.ShowEditBox = true;
+            dlg1.ShowBothFilesAndFolders = false;
+            dlg1.ShowFullPathInEditBox = true;
+            dlg1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            // Show the FolderBrowserDialog.
+            DialogResult result = dlg1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var path = dlg1.SelectedPath;
+                Console.WriteLine(path);
+                DefaultPath.Text = path;
+                Properties.Settings.Default["Path"] = path;
+                Properties.Settings.Default.Save();
+
+            }
+        }
+            
     }
 }
